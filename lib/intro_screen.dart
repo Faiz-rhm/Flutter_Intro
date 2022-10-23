d18bd9f094eebc109ab750df37e061a7d405b4bd
Rahman <faiz199011@gmail.com>
2022-10-23 16:37:32 +0430

    up intro widget

--- lib/intro_screen.dart
+++ lib/intro_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intro/intro_widget.dart';
 
 class IntroScreen extends StatefulWidget {
   const IntroScreen({super.key});
@@ -9,17 +10,18 @@ class IntroScreen extends StatefulWidget {
 
 class _IntroScreenState extends State<IntroScreen> {
 
-  final PageController _pageController = PageController();
+
+  final PageController  _pageController = PageController();
 
   int _activePage = 0;
 
-  void onNextPage() {
-    if (_activePage < _pages.length - 1) {
-      _pageController.nextPage(duration: const Duration(milliseconds: 500), curve: Curves.linear);
+  void onNextPage(){
+    if(_activePage  < _pages.length - 1) {
+      _pageController.nextPage(duration: const Duration(milliseconds: 500), curve: Curves.linear,);
     }
   }
 
- final List<Map<String, dynamic>> _pages = [
+  final List<Map<String, dynamic>> _pages = [
   {
     'color': '#ffe24e',
     'title': 'Hmmm, Healthy food',
@@ -43,7 +45,6 @@ class _IntroScreenState extends State<IntroScreen> {
   },
 ];
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -57,14 +58,14 @@ class _IntroScreenState extends State<IntroScreen> {
                 _activePage = page;
               });
             },
-            itemBuilder: (BuildContext context, int index) {
-              return  IntroWidget(
-                color: _pages[index]['color']!,
-                title: _pages[index]['title']!,
-                description: _pages[index]['description']!,
-                image: _pages[index]['image']!,
-                skip: _pages[index]['skip']!,
-                onTap: onNextPage
+            itemBuilder: (BuildContext context, int index){
+              return IntroWidget(
+                color: _pages[index]['color'],
+                title: _pages[index]['title'],
+                description: _pages[index]['description'],
+                image: _pages[index]['image'],
+                skip: _pages[index]['skip'],
+                onTab: onNextPage,
               );
             }
           ),
@@ -76,28 +77,43 @@ class _IntroScreenState extends State<IntroScreen> {
               children: [
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
-                  children: _buildIndicator(),
-                ),
+                  children: _buildIndicator()
+                )
               ],
             ),
-          ),
+          )
+
         ],
       ),
     );
   }
 
-  Widget _indicatorTrue() {
+  List<Widget> _buildIndicator() {
+    final indicators =  <Widget>[];
+
+    for(var i = 0; i < _pages.length; i++) {
+
+      if(_activePage == i) {
+        indicators.add(_indicatorsTrue());
+      }else{
+        indicators.add(_indicatorsFalse());
+      }
+    }
+    return  indicators;
+  }
+
+  Widget _indicatorsTrue() {
     final String color;
     if(_activePage == 0){
       color = '#ffe24e';
-    } else if(_activePage == 1){
+    } else  if(_activePage ==  1) {
       color = '#a3e4f1';
-    }else {
+    } else {
       color = '#31b77a';
     }
 
     return AnimatedContainer(
-      duration: const Duration(milliseconds: 300),
+      duration: const Duration(microseconds: 300),
       height: 6,
       width: 42,
       margin: const EdgeInsets.only(right: 8),
@@ -108,126 +124,16 @@ class _IntroScreenState extends State<IntroScreen> {
     );
   }
 
-  Widget _indicatorFalse() {
+  Widget _indicatorsFalse() {
     return AnimatedContainer(
-      duration: const Duration(milliseconds: 300),
+      duration: const Duration(microseconds: 300),
       height: 8,
       width: 8,
       margin: const EdgeInsets.only(right: 8),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(50),
-        color: Colors.grey.shade300,
+        color: Colors.grey.shade100,
       ),
     );
   }
-
-  List<Widget> _buildIndicator() {
-    final indicators = <Widget>[];
-    for(var i = 0; i < _pages.length; i++) {
-      if (_activePage == i) {
-        indicators.add(_indicatorTrue());
-      } else {
-        indicators.add(_indicatorFalse());
-      }
-    }
-
-    return indicators;
-  }
-}
-
-class IntroWidget extends StatelessWidget {
-  const IntroWidget({super.key, required this.color, required this.title, required this.description, required this.skip, required this.image, required this.onTap});
-
-  final String color;
-  final String title;
-  final String description;
-  final bool skip;
-  final String image;
-  final VoidCallback onTap;
-
-  @override
-  Widget build(BuildContext context) {
-    return ColoredBox(
-      color: hexToColor(color),
-      child: Stack(children: [
-        Container(
-          height: MediaQuery.of(context).size.height / 1.86,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage(image),
-              fit: BoxFit.fill,
-            ),
-          ),
-        ),
-        Positioned(
-          bottom: 0,
-          right: 0,
-          left: 0,
-          child: Container(
-            height: MediaQuery.of(context).size.height / 2.16,
-            decoration: const BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(100),
-              ),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 45),
-              child: Column(
-                children: [
-                  const SizedBox(height: 62,),
-                  Text(title, style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),),
-                  const SizedBox(height: 16,),
-                  Text(description, style: const TextStyle(fontSize: 18, height: 1.5, color: Colors.grey), textAlign: TextAlign.center,)
-                ],
-              ),
-            ),
-          ),
-        ),
-        Positioned(
-          bottom: 20,
-          right: 0,
-          left: 0,
-          child: Padding(
-            padding: const EdgeInsets.all(24),
-            child: skip
-            ? Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                TextButton(
-                  onPressed : () {},
-                  child: const Text('Skip Now', style: TextStyle(color: Colors.black),),
-                ),
-                GestureDetector(
-                  onTap: onTap,
-                  child: Container(
-                    padding: const EdgeInsets.all(16),
-                    decoration: BoxDecoration(
-                      color: hexToColor(color),
-                      borderRadius: BorderRadius.circular(50),
-                    ),
-                    child: const Icon(Icons.arrow_circle_right, color: Colors.white, size: 42,),
-                  ),
-                )
-              ],)
-            : SizedBox(
-              height: 46,
-              child: MaterialButton(
-                color: hexToColor(color),
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-                onPressed: () {},
-                child: const Text('Get Started', style: TextStyle(color: Colors.white,),),
-              ),
-            )
-          ),
-        ),
-      ],),
-    );
-  }
-}
-
-Color hexToColor(String hex) {
-  assert(RegExp(r'^#([0-9a-fA-F]{6})|([0-9a-fA-F]{8})$').hasMatch(hex));
-
-  return Color(int.parse(hex.substring(1), radix: 16) + (hex.length == 7 ? 0xFF000000 : 0x00000000));
 }
